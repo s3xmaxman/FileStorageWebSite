4810b164325f9d4f2ca11c65eee3991142c0dc4d
s3xmaxman <badside3919@gmail.com>
2024-03-17 08:51:00 +0900

    Add file upload functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,26 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+  title: z.string().min(1).max(200),
+  file: z
+        .custom<FileList>((val) => val instanceof FileList, "Required")
+        .refine((files) => files.length > 0, "Required"),
+})
 
 
 export default function Home() {
@@ -26,18 +46,70 @@ export default function Home() {
     orgId ? { orgId } : "skip"
   );
   const createFile = useMutation(api.files.createFile);
+
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+      resolver: zodResolver(formSchema),
+      defaultValues: {
+        title: "",
+        file: undefined,
+      },
+    })
+
+    const fileRef = form.register("file")
+    
+    function onSubmit(values: z.infer<typeof formSchema>) {
+      // Do something with the form values.
+      // ✅ This will be type-safe and validated.
+      console.log(values)
+    }
+
   return (
         <main className="container mx-auto pt-12">
           <div className="flex justify-between items-center">
             <h1 className="text-4xl font-bold">Your Files</h1>
             <Dialog>
-            <DialogTrigger>Open</DialogTrigger>
+            <DialogTrigger>
+              <Button onClick={() => {if(!orgId) return; createFile({ name: "hello world", orgId })}}>Click Me!</Button>
+            </DialogTrigger>
             <DialogContent>
               <DialogHeader>
-                <DialogTitle>Are you absolutely sure?</DialogTitle>
+                <DialogTitle className="mb-8">Upload File Here</DialogTitle>
                 <DialogDescription>
-                  This action cannot be undone. This will permanently delete your account
-                  and remove your data from our servers.
+                <Form {...form}>
+                  <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                    <FormField
+                      control={form.control}
+                      name="title"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Title</FormLabel>
+                          <FormControl>
+                            <Input placeholder="shadcn" {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="file"
+                      render={() => (
+                        <FormItem>
+                          <FormLabel>File</FormLabel>
+                          <FormControl>
+                            <Input 
+                                type="file" 
+                                {...fileRef}             
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <Button type="submit">Submit</Button>
+                  </form>
+                </Form>
                 </DialogDescription>
               </DialogHeader>
             </DialogContent>
@@ -48,8 +120,6 @@ export default function Home() {
             </div>
           ))}
      
-          <Button onClick={() => {if(!orgId) return; createFile({ name: "hello world", orgId })}}>Click Me!</Button>
-
           </div>
         </main>
   );
